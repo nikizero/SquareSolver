03314b1ffc9748ac2ac360000f880de636dc9a30
nikizero <106468885+nikizero@users.noreply.github.com>
2024-08-21 23:59:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
-enum roots
+enum Roots
 {
     NO_SOLUTIONS    = 0,
     ONE_ROOT        = 1,
@@ -11,34 +11,47 @@ enum roots
     INFINITE_ROOTS  = 3,
 };
 
-roots solver(double a, double b, double c, double* x1, double* x2);
-void resultolis(roots numsol, double x1, double x2);
+struct Quadr
+{
+  double a;
+  double b;
+  double c;
+};
+
+struct Solution
+{
+    double x1;
+    double x2;
+    Roots num_roots;
+};
+
 int input (double* coeff);
-roots solvesquare (double a, double b, double c, double* x1, double* x2);
-roots solvelinear (double b, double c, double* x1);
-int testing (int TestNum, double a, double b, double c, double x1right, double x2right, int numsolright);
+void Itog(Solution answer);
+Roots Solver(Quadr coefs, Solution* answer);
+Roots SolveLinear (Quadr coefs, Solution* answer);
+Roots SolveSquare (Quadr coefs, Solution* answer);
 
 int main()
 {
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, t = 1; // assert, enum
-    input (&a);
-    input (&b);
-    input (&c);
+    Quadr coefs = {0, 0, 0};
+    Solution answer = {0, 0, NO_SOLUTIONS};
 
-    roots var = NO_SOLUTIONS;
+    input (&coefs.a);
+    input (&coefs.b);
+    input (&coefs.c);
 
-    roots numsol = solver (a, b, c, &x1, &x2);
-    resultolis (numsol, x1, x2);
+    answer.num_roots = Solver (coefs, &answer);
+    Itog (answer); // naming
 
-    // t = testing (1, 0, 0, 0, 0, 0, 3);
-    // t = testing (2, 1, 0, 0, 0, 0, 1);
-    // t = testing (3, 0, 2, -1, 0.5, 0, 1);
-    // t = testing (4, 1, 2, 1, -1, 0, 1);
-    // t = testing (5, 0, 1, 0, 0, 0, 1);
-    // t = testing (6, 0, 0, 1, 0, 0, 0);
-    // t = testing (7, 4, 3, 2, 0, 0, 0);
-    // t = testing (8, 4, 3, -1, 0.25, -1, 2);
-    // t = testing (9, 1, 2, 3, 0, 0, 0);
+    // testing (1, 0, 0, 0, 0, 0, 3);
+    // testing (2, 1, 0, 0, 0, 0, 1);
+    // testing (3, 0, 2, -1, 0.5, 0, 1);
+    // testing (4, 1, 2, 1, -1, 0, 1);
+    // testing (5, 0, 1, 0, 0, 0, 1);
+    // testing (6, 0, 0, 1, 0, 0, 0);
+    // testing (7, 4, 3, 2, 0, 0, 0);
+    // testing (8, 4, 3, -1, 0.25, -1, 2);
+    // testing (9, 1, 2, 3, 0, 0, 0);
 
 }
 
@@ -60,42 +73,42 @@ int input (double* coeff)
     return NO_SOLUTIONS;
 }
 
-void resultolis(roots numsol, double x1, double x2)
+void Itog(Solution answer)
 {
-    switch(numsol)
+    switch(answer.num_roots)
     {
-        case NO_SOLUTIONS: printf("# No solutions");
+        case NO_SOLUTIONS:      printf("# No solutions");
             break;
-        case ONE_ROOT: printf("# x1 = %lf\n", x1);
+        case ONE_ROOT:          printf("# x1 = %lf\n", answer.x1);
             break;
-        case TWO_ROOTS: printf("# x1 = %lf, x2 = %lf \n", x1, x2);
+        case TWO_ROOTS:         printf("# x1 = %lf, x2 = %lf \n", answer.x1, answer.x2);
             break;
-        case INFINITE_ROOTS: printf("# All real numbers");
+        case INFINITE_ROOTS:    printf("# All real numbers");
             break;
     }
 }
-roots solver(double a, double b, double c, double* x1, double* x2) // разбить на 2 функции, одна из которых будет решать квадратное уравнение, второе линейное, enum
-{
-    assert(x1); // assert(x1 != nullptr)
-    assert(x2);
 
-    if (a == 0)
+Roots Solver(Quadr coefs, Solution* answer)
+{
+    assert(answer);
+
+    if (coefs.a == 0)
     {
-        return solvelinear (b, c, x1);
+        return SolveLinear (coefs, answer);
     }
     else
     {
-        return solvesquare (a, b, c, x1, x2);
+        return SolveSquare (coefs, answer);
     }
 }
 
-roots solvelinear (double b, double c, double* x1)
+Roots SolveLinear (Quadr coefs, Solution* answer)
 {
-    assert(x1);
+    assert(answer);
 
-    if (b == 0)
+    if (coefs.b == 0)
     {
-        if (c == 0)
+        if (coefs.c == 0)
         {
             return INFINITE_ROOTS;
         }
@@ -106,47 +119,47 @@ roots solvelinear (double b, double c, double* x1)
     }
     else
     {
-        *x1 = -c / b;
+        answer -> x1 = -coefs.c / coefs.b;
         return ONE_ROOT;
     }
 }
 
-roots solvesquare (double a, double b, double c, double* x1, double* x2)
+Roots SolveSquare (Quadr coefs, Solution* answer)
 {
-    assert (x1);
-    assert (x2);
-    double D = b * b - 4 * a * c;
+    assert (answer);
+
+    double D = coefs.b * coefs.b - 4 * coefs.a * coefs.c;
 
     if (D < 0)
     {
         return NO_SOLUTIONS;
     }
-    if (D == 0)
+    else if (D == 0)
     {
-        *x1 = -b / (2 * a);
+        answer -> x1 = -coefs.b / (2 * coefs.a);
         return ONE_ROOT;
     }
     else
     {
-        *x1 = (-b + sqrt(D)) / (2 * a);
-        *x2 = (-b - sqrt(D)) / (2 * a);
+        answer -> x1 = (-coefs.b + sqrt(D)) / (2 * coefs.a);
+        answer -> x2 = (-coefs.b - sqrt(D)) / (2 * coefs.a);
         return TWO_ROOTS;
     }
 }
 
-int testing (int TestNum, double a, double b, double c, double x1right, double x2right, int numsolright)
-{
-    double x1 = 0, x2 = 0;
-    int numsol = solver (a, b, c, &x1, &x2);
-    if (numsol != numsolright || x1 != x1right || x2 != x2right)
-    {
-        printf("Test N %d Failed: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n"
-        "Right Test: x1right = %lf, x2right = %lf, numsolright = %d \n", TestNum, a, b, c, x1, x2, numsol, x1right, x2right, numsolright);
-        return 0;
-    }
-    else
-    {
-        printf("Test N %d correct: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n", TestNum, a, b, c, x1, x2, numsol);
-        return 1;
-    }
-}
+//int testing (int TestNum, double a, double b, double c, double x1right, double x2right, int numsolright)
+//{
+//    double x1 = 0, x2 = 0;
+//    int numsol = Solver (a, b, c, &x1, &x2);
+//    if (numsol != numsolright || x1 != x1right || x2 != x2right)
+//    {
+//        printf("Test N %d Failed: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n"
+//        "Right Test: x1right = %lf, x2right = %lf, numsolright = %d \n", TestNum, a, b, c, x1, x2, numsol, x1right, x2right, numsolright);
+//        return 0;
+//    }
+//    else
+//    {
+//        printf("Test N %d correct: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n", TestNum, a, b, c, x1, x2, numsol);
+//        return 1;
+//    }
+//}
