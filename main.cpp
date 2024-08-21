9162bbea7c2963ff9015c268f382b2d0b700d56c
nikizero <106468885+nikizero@users.noreply.github.com>
2024-08-21 11:34:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int solver(double a, double b, double c, double* x1, double* x2);
 void resultolis(int numsol, double x1, double x2);
 int input (double* coeff);
+int solvesquare (double a, double b, double c, double* x1, double* x2);
+int solvelinear (double a, double b, double c, double* x1, double* x2);
+
 
 int main()
 {
@@ -49,42 +52,53 @@ void resultolis(int numsol, double x1, double x2)
 }
 
 int solver(double a, double b, double c, double* x1, double* x2) // разбить на 2 функции, одна из которых будет решать квадратное уравнение, второе линейное, enum
+
 {   if (a==0)
     {
-        if (b==0)
+       return solvelinear(a, b, c, x1, x2);
+    }
+    else
+    {
+        return solvelinear(a, b, c, x1, x2);
+    }
+}
+
+int solvelinear (double a, double b, double c, double* x1, double* x2)
+{
+    if (b==0)
+    {
+        if (c==0)
         {
-            if (c==0)
-            {
-                return 3;
-            }
-            else
-            {
-                return 0;
-            }
+            return 3;
         }
         else
         {
-            *x1 = -c/b;
-            return 1;
+            return 0;
         }
     }
     else
     {
-        double D = b*b - 4*a*c;
-        if (D < 0)
-        {
-            return 0;
-        }
-        if (D == 0)
-        {
-            *x1 = -b/2/a;
-            return 1;
-        }
-        else
-        {
-            *x1 = (-b + sqrt(D))/2/a;
-            *x2 = (-b - sqrt(D))/2/a;
-            return 2;
-        }
+        *x1=-c/a;
+        return 1;
+    }
+}
+
+int solvesquare (double a, double b, double c, double* x1, double* x2)
+{
+    double D = b*b - 4*a*c;
+    if (D < 0)
+    {
+        return 0;
+    }
+    if (D == 0)
+    {
+        *x1 = -b/2/a;
+        return 1;
+    }
+    else
+    {
+        *x1 = (-b + sqrt(D))/2/a;
+        *x2 = (-b - sqrt(D))/2/a;
+        return 2;
     }
 }
