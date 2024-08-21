1894811f91c8a628068b1a66227f0c00fa2850fc
nikizero <106468885+nikizero@users.noreply.github.com>
2024-08-22 00:44:56 +0300

    all parts migrate to structure, next step automatic test inputing, through massive of structure

--- main.cpp
+++ main.cpp
@@ -25,24 +25,40 @@ struct Solution
     Roots num_roots;
 };
 
-int input (double* coeff);
-void Itog(Solution answer);
-Roots Solver(Quadr coefs, Solution* answer);
-Roots SolveLinear (Quadr coefs, Solution* answer);
-Roots SolveSquare (Quadr coefs, Solution* answer);
+//int testing (int TestNum, double a, double b, double c, double x1right, double x2right, int numsolright)
+
+struct Test
+{
+    int TestNum;
+    double a;
+    double b;
+    double c;
+    double x1right;
+    double x2right;
+    Roots numsolright;
+};
+
+int                Testing (Test etalon);
+int                Input (double* coeff);
+void               Itog(Solution answer);
+Roots Solver       (Quadr coefs, Solution* answer);
+Roots SolveLinear  (Quadr coefs, Solution* answer);
+Roots SolveSquare  (Quadr coefs, Solution* answer);
 
 int main()
 {
     Quadr coefs = {0, 0, 0};
     Solution answer = {0, 0, NO_SOLUTIONS};
+    Test etalon = {1, 0, 0, 0, 0, 0, INFINITE_ROOTS};
 
-    input (&coefs.a);
-    input (&coefs.b);
-    input (&coefs.c);
+    Input (&coefs.a);
+    Input (&coefs.b);
+    Input (&coefs.c);
 
     answer.num_roots = Solver (coefs, &answer);
     Itog (answer); // naming
 
+    Testing(etalon);
     // testing (1, 0, 0, 0, 0, 0, 3);
     // testing (2, 1, 0, 0, 0, 0, 1);
     // testing (3, 0, 2, -1, 0.5, 0, 1);
@@ -55,7 +71,7 @@ int main()
 
 }
 
-int input (double* coeff)
+int Input (double* coeff)
 {
     assert (coeff);
 
@@ -77,13 +93,13 @@ void Itog(Solution answer)
 {
     switch(answer.num_roots)
     {
-        case NO_SOLUTIONS:      printf("# No solutions");
+        case NO_SOLUTIONS:      printf("# No solutions \n");
             break;
-        case ONE_ROOT:          printf("# x1 = %lf\n", answer.x1);
+        case ONE_ROOT:          printf("# x1 = %lf \n", answer.x1);
             break;
         case TWO_ROOTS:         printf("# x1 = %lf, x2 = %lf \n", answer.x1, answer.x2);
             break;
-        case INFINITE_ROOTS:    printf("# All real numbers");
+        case INFINITE_ROOTS:    printf("# All real numbers \n");
             break;
     }
 }
@@ -147,19 +163,20 @@ Roots SolveSquare (Quadr coefs, Solution* answer)
     }
 }
 
-//int testing (int TestNum, double a, double b, double c, double x1right, double x2right, int numsolright)
-//{
-//    double x1 = 0, x2 = 0;
-//    int numsol = Solver (a, b, c, &x1, &x2);
-//    if (numsol != numsolright || x1 != x1right || x2 != x2right)
-//    {
-//        printf("Test N %d Failed: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n"
-//        "Right Test: x1right = %lf, x2right = %lf, numsolright = %d \n", TestNum, a, b, c, x1, x2, numsol, x1right, x2right, numsolright);
-//        return 0;
-//    }
-//    else
-//    {
-//        printf("Test N %d correct: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, numsol = %d\n", TestNum, a, b, c, x1, x2, numsol);
-//        return 1;
-//    }
-//}
+int Testing (Test etalon)
+{
+    Quadr coefs = {etalon.a, etalon.b, etalon.c};
+    Solution answer = {0, 0, NO_SOLUTIONS};
+    answer.num_roots = Solver (coefs, &answer);
+    if (answer.num_roots != etalon.numsolright || answer.x1 != etalon.x1right || answer.x2 != etalon.x2right)
+    {
+        printf("Test N %d Failed: a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf, num_roots = %d\n"
+        "Right Test: x1right = %lf, x2right = %lf, numsolright = %d \n", etalon.TestNum, etalon.a, etalon.b, etalon.c, answer.x1, answer.x2, answer.num_roots, etalon.x1right, etalon.x2right, etalon.numsolright);
+        return 0;
+    }
+    else
+    {
+        printf("Test N %d correct \n", etalon.TestNum);
+        return 1;
+    }
+}
