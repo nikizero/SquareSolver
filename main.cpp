cd4ebbda3a57d84cd899f1f3cae59b21c551b2dc
nikizero <106468885+nikizero@users.noreply.github.com>
2024-08-22 17:51:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char* argv[])
         {2, 1, 0,  0,  0,     0, ONE_ROOT},
         {3, 0, 2, -1,  0.5,   0, ONE_ROOT},
         {4, 1, 2,  1, -1,     0, ONE_ROOT},
-        {5, 0, 1,  0,  0,     0, ONE_ROOT},
+        {5, 0, 1,  0,  0,     0, ONE_ROOT},                 //
         {6, 0, 0,  1,  0,     0, NO_SOLUTIONS},
         {7, 4, 3,  2,  0,     0, NO_SOLUTIONS},
         {8, 4, 3, -1,  0.25, -1, TWO_ROOTS},
@@ -105,11 +105,13 @@ int main(int argc, char* argv[])
         Input (&coefs.a);
         Input (&coefs.b);
         Input (&coefs.c);
+        answer.num_roots = Solver (coefs, &answer);
+        Itog (answer); // naming
 
     }
     if (Mode == TEST)
     {
-        for (int i = 0; i <= numtests; i++)
+        for (int i = 0; i < numtests; i++)
         {
             Testing (etalon[i]);
         }
